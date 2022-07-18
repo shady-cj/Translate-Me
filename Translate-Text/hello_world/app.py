2096122338a95d962ca085ce856270aa57f6f81f
shady-cj <petersp2000@gmail.com>
2022-07-18 02:31:49 +0100

    Completed

--- Translate-Text/hello_world/app.py
+++ Translate-Text/hello_world/app.py
@@ -6,9 +6,9 @@ def lambda_handler(event, context):
     
     client = boto3.client("translate", region_name="us-east-1")
     if "body" in event:
-        body = json.loads(event["body"])
-    phrase = body.get("phrase")
-    lang_code = body.get("lang_code")
+        event = json.loads(event["body"])
+    phrase = event.get("phrase")
+    lang_code = event.get("lang_code")
     result = client.translate_text(Text=phrase, SourceLanguageCode="auto", TargetLanguageCode=lang_code)
     translation = result["TranslatedText"]
 
